9a5b308d6e9dfd99bbd03248e9b9a1a674163097
Raúl <raul.galan.gomez@gmail.com>
2017-02-02 17:58:15 +0100

    Conf Update

--- microservice-mongo-comments/src/main/java/com/paradigma/demo/mongodb/CommentsDemoMongoApplication.java
+++ microservice-mongo-comments/src/main/java/com/paradigma/demo/mongodb/CommentsDemoMongoApplication.java
@@ -1,30 +1,15 @@
 package com.paradigma.demo.mongodb;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-
-import com.mongodb.Mongo;
-import com.mongodb.MongoClientOptions;
-
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Version;
 
 @Configuration
+@ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -55,43 +40,6 @@ public class CommentsDemoMongoApplication {
     	}
     	
     	
-    }
-    
-    @Autowired
-    private Environment env;
-    
-    @Autowired
-    private MongoProperties properties;
-
-    @Autowired(required = false)
-    private MongoClientOptions options;
-
-    @Bean(destroyMethod = "close")
-    public Mongo mongo(MongodProcess mongodProcess) throws IOException {
-        Net net = mongodProcess.getConfig().net();
-        properties.setHost(net.getServerAddress().getHostName());
-        properties.setPort(net.getPort());
-        return properties.createMongoClient(this.options,env);
-    }
-
-    @Bean(destroyMethod = "stop")
-    public MongodProcess mongodProcess(MongodExecutable mongodExecutable) throws IOException {
-        return mongodExecutable.start();
-    }
-
-    @Bean(destroyMethod = "stop")
-    public MongodExecutable mongodExecutable(MongodStarter mongodStarter, IMongodConfig iMongodConfig) throws IOException {
-        return mongodStarter.prepare(iMongodConfig);
-    }
-
-    @Bean
-    public IMongodConfig mongodConfig() throws IOException {
-        return new MongodConfigBuilder().version(Version.Main.PRODUCTION).build();
-    }
-
-    @Bean
-    public MongodStarter mongodStarter() {
-        return MongodStarter.getDefaultInstance();
     }
 
     
